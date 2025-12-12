df6fd8b1b14e898e658bf6dc1e9ba01ffac83def
Eduardo C Santos <eduardocarolinosantos2510@gmail.com>
2025-12-12 17:26:22 -0300

    Formatando Strings

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,17 @@ public class Main {
         int anoDeLancamento = 2022;
         System.out.println("Ano de lançamento:" + anoDeLancamento);
         boolean incluidoNoPlano = true;
-        double notaDoFilme = 9.8;
-
+        //Média cálculada pelas 3 notas do Alan, Rk e Edu.
+        double media = (9.8 + 6.3 + 7.0) /3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                  Filme Top Gun
+                  Filme de aventura com galã dos anos 80
+                  Muito bom!
+                  Ano de lançamento  
+                  """ + anoDeLancamento;
+        System.out.println(sinopse);
 
 
     }
