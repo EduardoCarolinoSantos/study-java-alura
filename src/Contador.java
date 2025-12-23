407af26e22c76b518d0e18a2a8f758bb13231173
Eduardo C Santos <eduardocarolinosantos2510@gmail.com>
2025-12-23 08:56:33 -0300

    Desafio: Criando um jogo de adivinhação.

---
+++ src/Contador.java
@@ -0,0 +1,11 @@
+public class Contador {
+    static void main() {
+
+        for (int contador =0; contador <=10; contador++){
+            System.out.println(contador);
+            if (contador >=10){
+                System.out.println("Parabéns, você sabe contar de 0 a 10 em Java");
+            }
+        }
+    }
+}
