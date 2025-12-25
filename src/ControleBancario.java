e80c662cfed1cd716eb412d7ed87f888fe391e2c
Edu.Santos.Dev <161091487+EduardoCarolinoSantos@users.noreply.github.com>
2025-12-25 20:52:21 -0300

    Montando o menu principal

--- src/ControleBancario.java
+++ src/ControleBancario.java
@@ -1,27 +1,47 @@
 import java.util.Scanner;
 
 public class ControleBancario {
-    static void main() {
-        Scanner scanner = new Scanner(System.in);
-        String inicio = String.format(""" 
-                Olá! Vamos iniciar o seu cadastro em nosso sistema?
-                Digite o seu nome: """);
-        System.out.println(inicio);
-        String nome = scanner.nextLine();
-        System.out.println("\nObrigado " + nome + " agora vamos para os próximos passos.");
 
-        String tipoConta = "Corrente";
-        Scanner saldoInicial = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("""
+                Olá! Vamos iniciar o seu cadastro em nosso sistema?
+                Digite o seu nome:
+                """);
+
+        String nome = scanner.nextLine();
+        System.out.println("\nObrigado " + nome + ", agora vamos para os próximos passos.");
+
+        System.out.print("Digite o saldo inicial: ");
         double saldoAtual = scanner.nextDouble();
 
+        String tipoConta = "Corrente";
+
         System.out.println("**********************************");
-        String extratoBancario = String.format("""
+        System.out.printf("""
                 Nome do cliente: %s
                 Tipo de conta: %s
                 Saldo atual: R$ %.2f
-                **********************************""",nome, tipoConta, saldoAtual);
-        System.out.println(extratoBancario);
+                **********************************
+                """, nome, tipoConta, saldoAtual);
 
+        int opcao = 0;
 
+        while (opcao != 4) {
+            System.out.println("""
+                    ** Digite sua opção **
+                    1 - Consultar saldo
+                    2 - Pix
+                    3 - Receber valor
+                    4 - Sair
+                    """);
+
+            opcao = scanner.nextInt();
+        }
+
+        System.out.println("Sistema encerrado.");
+        scanner.close();
     }
 }
